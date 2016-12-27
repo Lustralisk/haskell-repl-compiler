1ea9a4f3728905aaffb032a330cd3161620d7938
Lustralisk <wzlsc_lustre@163.com>
2016-12-28 01:05:40 +0800

    complete data value

--- final/src/Eval.hs
+++ final/src/Eval.hs
@@ -7,58 +7,58 @@ import Data.Either
 import Data.Attoparsec.Text
 import Parser
 
-data Value = B Bool
-           | C Char
-           | D Double
-           | L [Value]
+data Value = BoolValue Bool
+           | CharValue Char
+           | DoubleValue Double
+           | ListValue [Value]
 
 {- Divide eval -}
-evalParser :: Expr -> Either Bool Double
-evalParser (Number e) = Right e
-evalParser FalseLit = Left False
-evalParser TrueLit = Left True
-evalParser (Add e1 e2) = Right (n1 + n2)
-    where (Right n1) = evalParser e1
-          (Right n2) = evalParser e2
-evalParser (Sub e1 e2) = Right (n1 - n2)
-    where (Right n1) = evalParser e1
-          (Right n2) = evalParser e2
-evalParser (Mul e1 e2) = Right (n1 * n2)
-    where (Right n1) = evalParser e1
-          (Right n2) = evalParser e2
-evalParser (Div e1 e2) = Right (n1 / n2)
-    where (Right n1) = evalParser e1
-          (Right n2) = evalParser e2
+evalParser :: Expr -> Value
+evalParser (Number e) = DoubleValue e
+evalParser FalseLit = BoolValue False
+evalParser TrueLit = BoolValue True
+evalParser (Add e1 e2) = DoubleValue (n1 + n2)
+    where (DoubleValue n1) = evalParser e1
+          (DoubleValue n2) = evalParser e2
+evalParser (Sub e1 e2) = DoubleValue (n1 - n2)
+    where (DoubleValue n1) = evalParser e1
+          (DoubleValue n2) = evalParser e2
+evalParser (Mul e1 e2) = DoubleValue (n1 * n2)
+    where (DoubleValue n1) = evalParser e1
+          (DoubleValue n2) = evalParser e2
+evalParser (Div e1 e2) = DoubleValue (n1 / n2)
+    where (DoubleValue n1) = evalParser e1
+          (DoubleValue n2) = evalParser e2
 
-evalParser (And e1 e2) = Left (n1 && n2)
-    where (Left n1) = evalParser e1
-          (Left n2) = evalParser e2
-evalParser (Or e1 e2) = Left (n1 || n2)
-    where (Left n1) = evalParser e1
-          (Left n2) = evalParser e2
-evalParser (Not e) = Left (not n)
-    where (Left n) = evalParser e
-evalParser (Eq e1 e2) = Left (n1 == n2)
-    where (Right n1) = evalParser e1
-          (Right n2) = evalParser e2
-evalParser (Lw e1 e2) = Left (n1 < n2)
-    where (Right n1) = evalParser e1
-          (Right n2) = evalParser e2
-evalParser (Le e1 e2) = Left (n1 <= n2)
-    where (Right n1) = evalParser e1
-          (Right n2) = evalParser e2
-evalParser (Gr e1 e2) = Left (n1 > n2)
-    where (Right n1) = evalParser e1
-          (Right n2) = evalParser e2
-evalParser (Ge e1 e2) = Left (n1 >= n2)
-    where (Right n1) = evalParser e1
-          (Right n2) = evalParser e2
+evalParser (And e1 e2) = BoolValue (n1 && n2)
+    where (BoolValue n1) = evalParser e1
+          (BoolValue n2) = evalParser e2
+evalParser (Or e1 e2) = BoolValue (n1 || n2)
+    where (BoolValue n1) = evalParser e1
+          (BoolValue n2) = evalParser e2
+evalParser (Not e) = BoolValue (not n)
+    where (BoolValue n) = evalParser e
+evalParser (Eq e1 e2) = BoolValue (n1 == n2)
+    where (DoubleValue n1) = evalParser e1
+          (DoubleValue n2) = evalParser e2
+evalParser (Lw e1 e2) = BoolValue (n1 < n2)
+    where (DoubleValue n1) = evalParser e1
+          (DoubleValue n2) = evalParser e2
+evalParser (Le e1 e2) = BoolValue (n1 <= n2)
+    where (DoubleValue n1) = evalParser e1
+          (DoubleValue n2) = evalParser e2
+evalParser (Gr e1 e2) = BoolValue (n1 > n2)
+    where (DoubleValue n1) = evalParser e1
+          (DoubleValue n2) = evalParser e2
+evalParser (Ge e1 e2) = BoolValue (n1 >= n2)
+    where (DoubleValue n1) = evalParser e1
+          (DoubleValue n2) = evalParser e2
 
 
 
-eval :: Text -> Either Bool Double
+eval :: Text -> Value
 eval t = let (Right expr) = (parseOnly exprParser t) in evalParser expr
 
-printEval :: Either Bool Double -> String
-printEval (Left a) = show a
-printEval (Right a) = show a
+printEval :: Value -> String
+printEval (BoolValue a) = show a
+printEval (DoubleValue a) = show a
