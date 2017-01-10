278cd7635bdb2b7fd0594f94714b1bc83a8b0198
dycraft <dy_private@163.com>
2017-01-11 01:48:58 +0800

    recover statement & function.

--- final/src/Eval.hs
+++ final/src/Eval.hs
@@ -29,7 +29,7 @@ data EvalError = TypeError String
 
 instance Show EvalError where
     show (TypeError msg) = "TypeError: " ++ msg
-    show (NotFoundError msg) = "VariableNotFoundError: " ++ msg
+    show (NotFoundError msg) = "NotFoundError: " ++ msg
     show (OtherError msg) = msg
 
 type EvalMonad = Either EvalError
@@ -112,11 +112,11 @@ evalExprParser _ (Number e) = Right (DoubleValue e)
 evalExprParser _ (CharLit e) = Right (CharValue e)
 evalExprParser _ FalseLit = Right (BoolValue False)
 evalExprParser _ TrueLit = Right (BoolValue True)
--- evalExprParser env (Function t es) = case M.lookup t env of
---     Just (FunctionValue ts stat env') -> case M.lookup "$$result$$" env'' of
---         Nothing -> Left ""
---         Just v -> Right v
---         where env'' = evalStatementParser (inject env' env ts es) stat
+evalExprParser env (Function t es) = case M.lookup t env of
+    Just (FunctionValue ts stat env') -> case M.lookup "$$result$$" env'' of
+        Nothing -> Left (NotFoundError "No such function")
+        Just v -> Right v
+        where env'' = evalStatementParser (inject env' env ts es) stat
 evalExprParser env (Let t e1 e2) = evalExprParser env' e2 where
     env' = updateM t (evalExprParser env e1) env
 {- Not finished yet -}
@@ -126,6 +126,7 @@ evalExprParser env (LambdaCall e1 e2) = evalExprParser env (Let t e2 e3)
 {- Need to check whether e1, e2 are Double -}
 {- Need to due with Inf -}
 
+-- !!! Abandon:
 -- evalExprParser env (Add e1 e2) = case (evalExprParser env e1, evalExprParser env e2) of
 --     (Right (DoubleValue v1), Right (DoubleValue v2)) -> Right (DoubleValue (v1 + v2))
 --     (Right (DoubleValue v1), Right _) -> Left "Add: right operand type error"
@@ -219,7 +220,7 @@ evalExprParser env (Car (Cons e _)) = v
     where v = evalExprParser env e
 evalExprParser env (Cdr (Cons _ e)) = v
     where v = evalExprParser env e
-{-
+
 -------------------------------------------------------------------------------
 --- evalStatementParser
 --- Execute Statement based on given Env and may effect on it,
@@ -232,27 +233,28 @@ evalStatementParser env (StatementList s) = case s of
         (Return e) -> updateM "$$result$$" (evalExprParser env e) env
         _ -> evalStatementParser env' (StatementList xs) where
             env' = evalStatementParser env x
-evalStatementParser env (Set t e) = (updateM t (evalExprParser env e) env)
+evalStatementParser env (Set t e) = updateM t (evalExprParser env e) env
 evalStatementParser env Skip = env
 {- Need to check whether e is Bool -}
 evalStatementParser env (If e s1 s2) = case evalExprParser env e of
-    BoolValue True -> evalStatementParser env s1
-    BoolValue False -> evalStatementParser env s2
+    Right (BoolValue True) -> evalStatementParser env s1
+    Right (BoolValue False) -> evalStatementParser env s2
 {- Need to check whether e is Bool -}
 evalStatementParser env (While e s) = case evalExprParser env e of
-    BoolValue True -> case M.lookup "$$result$$" env of
+    Right (BoolValue True) -> case M.lookup "$$result$$" env of
         Just v -> env
         Nothing -> evalStatementParser env' (While e s) where
             env' = evalStatementParser env s
-    BoolValue False -> env
+    Right (BoolValue False) -> env
 {- Need to check whether e is Int using isInt at L19 -}
-evalStatementParser env (MakeVector t e) = (updateM t (ListValue [(DoubleValue 0) | i <- [1..length]]) env) where
-    (DoubleValue length') = evalExprParser env e
+evalStatementParser env (MakeVector t e) = updateM t (Right (ListValue [DoubleValue 0 | i <- [1..length]])) env where
+    Right (DoubleValue length') = evalExprParser env e
     length = fromEnum length'
 {- Need to check whether e1, e2 are Int using isInt at L19 -}
 evalStatementParser env (SetVector t e1 e2) = case M.lookup t env of
-    Just (ListValue v) -> updateM t (ListValue ((Prelude.take i v) ++ [evalExprParser env e2] ++ (Prelude.drop (i + 1) v))) env where
-        (DoubleValue i') = evalExprParser env e1
+    Just (ListValue v) -> updateM t (Right (ListValue (Prelude.take i v ++ [v'] ++ Prelude.drop (i + 1) v))) env where
+        Right (DoubleValue i') = evalExprParser env e1
+        Right v' = evalExprParser env e2
         i = fromEnum i'
 evalStatementParser env (Return e) = updateM "$$result$$" (evalExprParser env e) env
 
@@ -261,30 +263,26 @@ evalStatementParser env (Return e) = updateM "$$result$$" (evalExprParser env e)
 -------------------------------------------------------------------------------
 evalFunctionParser :: Env -> Function -> Env
 evalFunctionParser env (Def t ts stat) = env' where
-        env' = updateM t (FunctionValue ts stat env') env
--}
+        env' = updateM t (Right (FunctionValue ts stat env')) env
 
 
 evalExpr :: Env -> [Char] -> EvalMonad Value
 evalExpr env t = let (Right expr) = (parseOnly exprParser (pack t)) in evalExprParser env expr
 
--- evalStatement :: Env -> [Char] -> Env
--- evalStatement env line = evalStatementParser env statement where
---     (Right statement) = parseOnly statementParser $ pack line
---
--- evalFunction :: Env -> [Char] -> Env
--- evalFunction env line = evalFunctionParser env function where
---     (Right function) = parseOnly functionParser $ pack line
---
--- eval :: Env -> [Char] -> (Env, [Char])
--- eval env line = case parseOnly functionParser $ pack line of
---     (Right function) -> (evalFunctionParser env function, "")
---     _ -> case parseOnly statementParser $ pack line of
---         (Right statement) -> (evalStatementParser env statement, "")
---         _ -> (env, printEvalExpr $ evalExpr env line)
+evalStatement :: Env -> [Char] -> Env
+evalStatement env line = evalStatementParser env statement where
+    (Right statement) = parseOnly statementParser $ pack line
+
+evalFunction :: Env -> [Char] -> Env
+evalFunction env line = evalFunctionParser env function where
+    (Right function) = parseOnly functionParser $ pack line
 
 eval :: Env -> [Char] -> (Env, [Char])
-eval env line = (env, printEvalExpr $ evalExpr env line)
+eval env line = case parseOnly functionParser $ pack line of
+    (Right function) -> (evalFunctionParser env function, "")
+    _ -> case parseOnly statementParser $ pack line of
+        (Right statement) -> (evalStatementParser env statement, "")
+        _ -> (env, printEvalExpr $ evalExpr env line)
 
 {- Need to somewhat refine -}
 printEvalExpr :: EvalMonad Value -> String
