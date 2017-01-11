72b07c17899e46f986884b95df1dbd04effab91e
dycraft <dy_private@163.com>
2017-01-11 22:19:01 +0800

    try spec.

--- final/test/Specs/EvalSpec.hs
+++ final/test/Specs/EvalSpec.hs
@@ -3,5 +3,24 @@
 module Specs.EvalSpec where
 
 import Test.QuickCheck
+import Parser
+import Eval
 
 evalTests = []
+
+instance Arbitrary Expr where
+    arbitrary = choose ()
+
+-- instance Arbitrary Expr where
+--     arbitrary = sized arbExpr
+--
+-- arbExpr :: Int -> Gen Expr
+-- arbExpr 0 = do
+--     a <- arbitrary
+--     return $ Node a []
+-- arbExpr n = do
+--     (Positive m) <- arbitrary
+--     let n' = n `div` (m + 1)
+--     f <- replicateM m (arbTree n')
+--     a <- arbitrary
+--     return $ Node a f
