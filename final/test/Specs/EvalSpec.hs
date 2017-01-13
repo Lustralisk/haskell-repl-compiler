8a6917d9b12e426f27cc955964bc7902e2db3687
dycraft <dy_private@163.com>
2017-01-13 13:59:43 +0800

    a pace of Gen Expr.

--- final/test/Specs/EvalSpec.hs
+++ final/test/Specs/EvalSpec.hs
@@ -1,26 +1,36 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module Specs.EvalSpec where
+module Specs.EvalSpec (evalTests) where
 
 import Test.QuickCheck
+import qualified Data.Map as M
 import Parser
-import Eval
-
-evalTests = []
+import EvalT
 
 instance Arbitrary Expr where
-    arbitrary = choose ()
+    arbitrary = sized genExpr
+    -- shrink = shrinkExpr
 
--- instance Arbitrary Expr where
---     arbitrary = sized arbExpr
---
--- arbExpr :: Int -> Gen Expr
--- arbExpr 0 = do
---     a <- arbitrary
---     return $ Node a []
--- arbExpr n = do
---     (Positive m) <- arbitrary
---     let n' = n `div` (m + 1)
---     f <- replicateM m (arbTree n')
---     a <- arbitrary
---     return $ Node a f
+lits = [TrueLit, FalseLit]
+uniOpers = [Not]
+binOpers = [And, Or]
+opers e1 e2 = [Not e1, And e1 e2, Or e1 e2]
+
+genExpr :: Int -> Gen Expr
+genExpr 0 = elements lits
+genExpr n = do
+    e1 <- genExpr (n - 1)
+    e2 <- genExpr (n - 1)
+    elements $ opers e1 e2
+
+-- shrinkExpr :: Expr -> [Expr]
+-- shrinkExpr TrueLit = []
+-- shrinkExpr FalseLit = []
+-- shrinkExpr (Not e) = lits ++ shrinkTree e
+
+prop_ResultIsBoolValue :: String -> Bool
+prop_ResultIsBoolValue expr = (evalExpr expr) >>= isBoolValue where
+    isBoolValue (BoolValue _) = True
+    isBoolValue _ = False
+
+evalTests = [("ResultIsBoolValue", quickCheck prop_ResultIsBoolValue)]
--- final/test/Specs/ParserSpec.hs
+++ final/test/Specs/ParserSpec.hs
@@ -1,15 +1,10 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module Specs.ParserSpec where
+module Specs.ParserSpec (parserTests) where
 
 import Test.QuickCheck
 import Data.Text
 import Data.Attoparsec.Text
 import Parser
 
-prop_falseParser_skipSpace :: Text -> Property
-prop_falseParser_skipSpace text = (isInfixOf "False" text) ==> parseOnly falseParser text == Right FalseLit
-
-falseParserTests = [("prop_falseParser_skipSpace", prop_falseParser_skipSpace)]
-
-parserTests = falseParserTests
+parserTests = []
