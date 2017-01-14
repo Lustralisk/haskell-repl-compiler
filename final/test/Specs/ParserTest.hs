8dd51be101dbf87417f209ff9ef05e7c3b32ad7a
dycraft <dy_private@163.com>
2017-01-14 16:09:27 +0800

    complete ParserTest

--- final/test/Specs/ParserTest.hs
+++ final/test/Specs/ParserTest.hs
@@ -15,8 +15,9 @@ genBoolLit =  elements [BoolLit True, BoolLit False]
 genInt :: Gen Expr
 genInt = Number . toEnum <$> (choose (0, 100) :: Gen Int)
 
-genChar :: Gen Char
-genChar = arbitrary
+genChar :: Gen Expr
+-- genChar = CharLit <$> arbitrary
+genChar = CharLit <$> elements (['A'..'Z'] ++ ['a'..'z'])
 
 genDouble :: Gen Expr
 genDouble = Number <$> choose (0, 100.0)
@@ -25,7 +26,7 @@ genNil :: Gen Expr
 genNil = return Nil
 
 genVarName :: Gen Text
-genVarName = pack <$> listOf1 elements ['a'..'z']
+genVarName = pack <$> listOf1 (elements ['a'..'z'])
 
 genVar :: Gen Expr
 genVar = Variable <$> genVarName
@@ -33,37 +34,34 @@ genVar = Variable <$> genVarName
 genBooleanExpr :: Int -> Gen Expr
 genBooleanExpr limit = sized genN where
     genN 0 = genBoolLit
-    genN 1 = genBoolLit
     genN n = do
         e1 <- genN (min limit n - 1)
-        e2 <- genN (min limit n - 2)
+        e2 <- genN (min limit n - 1)
         elements [Not e1, And e1 e2, Or e1 e2]
 
 genNumberExpr :: Int -> Gen Expr
 genNumberExpr limit = sized genN where
     genN 0 = oneof [genInt, genDouble]
-    genN 1 = oneof [genInt, genDouble]
     genN n = do
         e1 <- genN (min limit n - 1)
-        e2 <- genN (min limit n - 2)
+        e2 <- genN (min limit n - 1)
         elements [Add e1 e2, Sub e1 e2, Mul e1 e2, Div e1 e2]
 
 genExpr :: Int -> Gen Expr
 genExpr limit = sized genN where
     genN 0 = oneof [genInt, genDouble, genBoolLit, genChar, genNil, genVar]
-    genN 1 = oneof [genInt, genDouble, genBoolLit, genChar, genNil, genVar]
     genN n = frequency [(1, genUniExpr), (3, genBinExpr)] where
         genBinExpr = do
             e1 <- genN (min limit n - 1)
-            e2 <- genN (min limit n - 2)
+            e2 <- genN (min limit n - 1)
             t <- genVarName
             elements [Add e1 e2, Sub e1 e2, Mul e1 e2, Div e1 e2, And e1 e2, Or e1 e2,
                 Eq e1 e2, Lw e1 e2, Le e1 e2, Gr e1 e2, Ge e1 e2, Cons e1 e2,
-                Let t e1 e2, Function t [e1, e2], LambdaCall e1 e2]
+                Let t e1 e2, Function t [e1, e2]]
         genUniExpr = do
             e <- genN (min limit n - 1)
             t <- genVarName
             elements [Not e, Car e, Cdr e, Vec t e, Function t [e], Lambda t e]
 
 prop_ShowParse :: Property
-prop_ShowParse = forAll (genExpr 100) $ \x -> parseOnly exprParser $ pack $ show x
+prop_ShowParse = forAll (genExpr 6) $ \x -> parseOnly exprParser (pack $ show x) == Right x
