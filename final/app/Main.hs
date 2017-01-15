17c7ba93d28abb60004b2b130d60733fe9a64e32
ocNflag <oc574769447@gmail.com>
2017-01-15 21:45:45 +0800

    sth wrong

--- final/app/Main.hs
+++ final/app/Main.hs
@@ -14,5 +14,6 @@ main = do
         ["-i", file1, "-o", file2] -> return () -- 解释执行
         ["-t", file1, "-o", file2] -> return () -- 输出语法树
         [file1, "-o", file2] -> translateLang file1 file2 -- 编译
+        [file1, "-ir"] -> execWrapper file1 -- 编译
         ["-repl"] -> runReplT
 --main = execWrapper "/Users/ocNflag/Desktop/out.txt"
--- final/src/Compiler/CompExec.hs
+++ final/src/Compiler/CompExec.hs
@@ -4,6 +4,7 @@ module Compiler.CompExec where
 
 import Control.Applicative
 import Data.Functor
+import Data.Text
 import Data.Either
 import Control.Monad
 import qualified Data.Map as M
@@ -11,7 +12,14 @@ import qualified Data.Set as S
 
 import System.IO
 import Parser
+import Eval
 import Compiler.Comp
+
+charCount :: Char -> String -> Int
+charCount c s = Data.Text.count (pack [c]) (pack s)
+
+newCount :: Int -> String -> Int
+newCount i s = i + charCount '(' s - charCount ')' s
 
 getMemoSet :: AvailMemo
 getMemoSet = getMemoSet' 255 S.empty
@@ -31,18 +39,13 @@ translateLang input_path output_path = do
 executeLoop :: CompTable -> String -> Int -> Handle -> Handle -> IO ()
 executeLoop cpt hist cnt inh ouh = do
     isEof <- hIsEOF inh
-    if isEof
-        then return ()
+    if not isEof
+        then do
+            line <- hGetLine inh
+            executeLoop cpt (hist ++ " " ++ line) (newCount cnt line) inh ouh
     else do
-        line <- hGetLine inh
-        case (hist ++ " " ++ line) of
-            _ -> case out of
-                "" -> executeLoop cpt' "" 0 inh ouh
-                _ -> do
-                    putStrLn line'
-                    --print cpt'
-                    hPutStr ouh out
-                    executeLoop cpt' "" 0 inh ouh
-                where
-                    line' = hist ++ " " ++ line
-                    (cpt', out) = comp cpt line'
+            putStrLn hist
+            --print cpt'
+            hPutStr ouh out
+            where
+                (cpt', out) = comp cpt hist
